0c19f36baa82ea829999fd4b2a9499f8560e42eb
DeveloperRaghavan <developer.raghavan@gmail.com>
2022-09-26 17:10:22 +0530

    PushNotification

--- Admin/javascript/Dashboard.js
+++ Admin/javascript/Dashboard.js
@@ -2,8 +2,9 @@ firebase.auth().onAuthStateChanged((user)=>{
     if(!user){
         location.replace("index.html")
     }else{
-        document.getElementById("user").innerHTML = "Hello, "+user.email
+        document.getElementById("user").innerHTML = "Hello, "+user.email    
     }
+    
 })
 
 
